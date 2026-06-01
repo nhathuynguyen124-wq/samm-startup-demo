5b0d1fca5fc5e3ec2872956c2a5eedbc357216e5
nhathuynguyen124-wq <nhathuynguyen124@gmail.com>
2026-06-01 17:15:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,12 @@ import os
 import sys
 
 # GIẢI PHÁP AN TOÀN: Lấy thông tin từ biến môi trường (Environment Variables)
-# Không còn hardcode mật khẩu trực tiếp trong code nữa!
 DATABASE_HOST = os.environ.get("DB_HOST", "192.168.1.250")
 DATABASE_USER = os.environ.get("DB_USER", "sa_admin")
-DATABASE_PASSWORD = os.environ.get("DB_PASSWORD") # Mật khẩu thật được giấu ở ngoài
+DATABASE_PASSWORD = os.environ.get("DB_PASSWORD") 
 
 def connect_to_database():
     print(f"[*] Đang khởi tạo kết nối đến Cơ sở dữ liệu tại {DATABASE_HOST}...")
-    
-    # Kiểm tra xem hệ thống đã cấu hình biến môi trường an toàn chưa
     if DATABASE_PASSWORD:
         print("[+] KẾT NỐI THÀNH CÔNG! Hệ thống bảo mật đạt chuẩn SAMM Cấp độ 2.")
         return True
